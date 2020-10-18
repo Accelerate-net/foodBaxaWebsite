6cd658dd0afc08bbb7ed30f40b9bea9c5e7ee172
Akshay Krishna <akshaykrishnakanrh@gmail.com>
2020-10-19 01:46:51 +0530

    Landing Page added

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// $(document).ready(function(){
+//     $('#show1').fadeIn('300');
+//     $("[id^=show]").each(function(index) {
+//         $(this).delay(400*index).fadeIn(300);
+//     });
+// })
